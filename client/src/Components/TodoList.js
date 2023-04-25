1b7bf904d5d35d16049bdd464e3ef789de345dc9
dipesh shrestha <dipesh.shrestha@ebpearls.com>
2023-04-25 12:01:40 +0545

    delete button disabled when no items in list

--- client/src/Components/TodoList.js
+++ client/src/Components/TodoList.js
@@ -1,42 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import Todo from './Todo';
+import Todo from "./Todo";
 import Button from "@material-ui/core/Button";
-import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
 
-const TodoList = ({todos, setTodos, filteredTodos, setInputText}) => {
+const TodoList = ({ todos, setTodos, filteredTodos, setInputText }) => {
+  const handleDeleteAll = () => {
+    setTodos([]);
+  };
 
-    const handleDeleteAll = () =>{
-        setTodos([]);
-    }
+  return (
+    <div div="todo-container">
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <Todo
+            key={todo.id}
+            todos={todos}
+            todo={todo}
+            setTodos={setTodos}
+            text={todo.text}
+            setInputText={setInputText}
+          />
+        ))}
+      </ul>
+      {filteredTodos.length !== 0 && (
+        <Button
+          className="deleteall-btn"
+          style={{ margin: "0 auto", display: "flex" }}
+          variant="contained"
+          color="secondary"
+          onClick={handleDeleteAll}
+        >
+          <p>Delete All</p>
+          <DeleteOutlineIcon />
+        </Button>
+      )}
+    </div>
+  );
+};
 
-    return (
-        <div div="todo-container">
-            <ul className="todo-list">
-                {filteredTodos.map(todo => (
-                    <Todo 
-                        key={todo.id} 
-                        todos={todos}
-                        todo={todo}
-                        setTodos={setTodos} 
-                        text={todo.text}
-                        setInputText={setInputText} />
-                ))}
-            </ul>
-            <Button
-                    className="deleteall-btn"
-                    style={{margin: '0 auto',
-                    display: "flex"}}
-                    variant="contained"
-                    color="secondary"
-                    onClick={handleDeleteAll}
-                  >
-                      <p>Delete All</p>
-                    <DeleteOutlineIcon />
-                  </Button>
-            
-        </div>
-    )
-}
-
-export default TodoList
+export default TodoList;
